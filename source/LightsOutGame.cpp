66926b3c78854041d9070c15e3ce14d6c87219e5
gerecke.jason <killertofu@gmail.com>
2009-05-08 21:01:25 -0700

    Fix rendering bug. After selecting a light, all but it and the four other dirty lights would disappear. This was due to the gameboard not being transparent and overwriting the other lights with black.

--- source/LightsOutGame.cpp
+++ source/LightsOutGame.cpp
@@ -231,7 +231,20 @@ bool LightsOutGame::paint(SDL_Surface& surface, int width, int height) {
 	
 	//Create & paint gameboard subsurface
 	///////////////////////////////////////////////////
-	SDL_Surface* gameboard = SDL_CreateRGBSurface(SDL_HWSURFACE,this->surface->h,this->surface->h,16,0,0,0,0);
+	int rmask,gmask,bmask,amask;
+	#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	rmask = 0xff000000;
+	gmask = 0x00ff0000;
+	bmask = 0x0000ff00;
+	amask = 0x000000ff;
+	#else
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
+	#endif
+
+	SDL_Surface* gameboard = SDL_CreateRGBSurface(SDL_HWSURFACE,this->surface->h,this->surface->h,32,rmask,gmask,bmask,amask);
 	double tileWidth  = (double)gameboard->w/(double)lights->getWidth();
 	double tileHeight = (double)gameboard->h/(double)lights->getHeight();
 	
